a71a61871e6b8e32eeb8d49a508374b5be102cd1
Lucas Amberg <102396588+lucas-amberg@users.noreply.github.com>
2023-07-24 14:55:30 -0700

    Create basic update library function Begin work on updateLibrary function, will likely need a lot more work

--- script.js
+++ script.js
@@ -11,5 +11,22 @@ function addBookToLibrary(title, author, pages, read) {
     myLibrary.push(new Book(title, author, pages, read));
 }
 
+const libraryContainer = document.querySelector(".library-container");
+
 addBookToLibrary("The Hobbit", "J.R.R. Tolkein", 262, true);
 addBookToLibrary("Dune", "Frank Herbert", 896, false);
+
+function updateLibrary(libraryLength){
+    libraryContainer.innerHTML = "";
+    for (let i = 0; i < libraryLength; i++) {
+        let newLibraryEntry = libraryContainer.appendChild(document.createElement("div"));
+        let title = newLibraryEntry.appendChild(document.createElement("h2"));
+        let author = newLibraryEntry.appendChild(document.createElement("p"));
+        let pages = newLibraryEntry.appendChild(document.createElement("p"));
+        let read = newLibraryEntry.appendChild(document.createElement("button"));
+        title.textContent = myLibrary[i].title;
+        author.textContent = `By ${myLibrary[i].author}`;
+        pages.textContent = `${myLibrary[i].pages} pages`;
+        read.textContent = myLibrary[i].textContent === true ? "Read" : "Not Read";
+    }
+}

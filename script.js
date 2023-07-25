6ff5df5500f71a393a6efa4a7624c0c3dc92c539
Lucas Amberg <102396588+lucas-amberg@users.noreply.github.com>
2023-07-24 21:08:36 -0700

    Create functionality for the read button Make the read button update color and text on click, as well as the object's property for whether the user read or not

--- script.js
+++ script.js
@@ -12,13 +12,49 @@ function addBookToLibrary(title, author, pages, read) {
 }
 
 const libraryContainer = document.querySelector(".library-container");
+let readButton = document.querySelectorAll(".book-button")
 
 addBookToLibrary("The Hobbit", "J.R.R. Tolkein", 262, true);
 addBookToLibrary("Dune", "Frank Herbert", 896, false);
+addBookToLibrary("Dune", "Frank Herbert", 896, false);
+addBookToLibrary("Dune", "Frank Herbert", 896, false);
+addBookToLibrary("Dune", "Frank Herbert", 896, false);
+addBookToLibrary("The Hobbit", "J.R.R. Tolkein", 262, true);
+addBookToLibrary("Dune", "Frank Herbert", 896, false);
+addBookToLibrary("Dune", "Frank Herbert", 896, false);
+addBookToLibrary("Dune", "Frank Herbert", 896, false);
+addBookToLibrary("Dune", "Frank Herbert", 896, false);
+addBookToLibrary("The Hobbit", "J.R.R. Tolkein", 262, true);
+addBookToLibrary("Dune", "Frank Herbert", 896, false);
+addBookToLibrary("Dune", "Frank Herbert", 896, false);
+addBookToLibrary("Dune", "Frank Herbert", 896, false);
+addBookToLibrary("Dune", "Frank Herbert", 896, false);
+addBookToLibrary("The Hobbit", "J.R.R. Tolkein", 262, true);
+addBookToLibrary("Dune", "Frank Herbert", 896, false);
+addBookToLibrary("Dune", "Frank Herbert", 896, false);
+addBookToLibrary("Dune", "Frank Herbert", 896, false);
+addBookToLibrary("Dune", "Frank Herbert", 896, false);
+addBookToLibrary("The Hobbit", "J.R.R. Tolkein", 262, true);
+addBookToLibrary("Dune", "Frank Herbert", 896, false);
+addBookToLibrary("Dune", "Frank Herbert", 896, false);
+addBookToLibrary("Dune", "Frank Herbert", 896, false);
+addBookToLibrary("Dune", "Frank Herbert", 896, false);
+addBookToLibrary("The Hobbit", "J.R.R. Tolkein", 262, true);
+addBookToLibrary("Dune", "Frank Herbert", 896, false);
+addBookToLibrary("Dune", "Frank Herbert", 896, false);
+addBookToLibrary("Dune", "Frank Herbert", 896, false);
+addBookToLibrary("Dune", "Frank Herbert", 896, false);
+addBookToLibrary("The Hobbit", "J.R.R. Tolkein", 262, true);
+addBookToLibrary("Dune", "Frank Herbert", 896, false);
+addBookToLibrary("Dune", "Frank Herbert", 896, false);
+addBookToLibrary("Dune", "Frank Herbert", 896, false);
+addBookToLibrary("Dune", "Frank Herbert", 896, false);
 
-function updateLibrary(libraryLength){
+updateLibrary();
+
+function updateLibrary(){
     libraryContainer.innerHTML = "";
-    for (let i = 0; i < libraryLength; i++) {
+    for (let i = 0; i < myLibrary.length; i++) {
         let newLibraryEntry = libraryContainer.appendChild(document.createElement("div"));
         let title = newLibraryEntry.appendChild(document.createElement("h2"));
         let author = newLibraryEntry.appendChild(document.createElement("p"));
@@ -27,6 +63,35 @@ function updateLibrary(libraryLength){
         title.textContent = myLibrary[i].title;
         author.textContent = `By ${myLibrary[i].author}`;
         pages.textContent = `${myLibrary[i].pages} pages`;
-        read.textContent = myLibrary[i].textContent === true ? "Read" : "Not Read";
+        read.textContent = myLibrary[i].read === true ? "Read" : "Not Read";
+        
+        myLibrary[i].read === true ? 
+        read.classList.add("read-button") :
+        read.classList.add("unread-button");
+
+        read.classList.add("book-button");
+        read.id = (`${i}`);
+
+        readButton = document.querySelectorAll(".book-button");
+
+        
+
+        
+        newLibraryEntry.classList.add("library-book");
     }
+
+    readButton.forEach((item) => {
+        item.addEventListener("click", () => {
+            item.classList.toggle("read-button");
+            item.classList.toggle("unread-button");
+            item.textContent === "Read" ?
+            item.textContent = "Not Read" :
+            item.textContent = "Read";
+    
+            myLibrary[item.id].read === true ?
+            myLibrary[item.id].read = false :
+            myLibrary[item.id].read = true;
+        })
+    })
 }
+

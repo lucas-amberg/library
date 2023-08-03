fedc9374165ad8398ea9e50009fe0522a415c8ff
Lucas Amberg <102396588+lucas-amberg@users.noreply.github.com>
2023-08-03 13:30:41 -0700

    Change book to a class

--- script.js
+++ script.js
@@ -1,11 +1,13 @@
 let myLibrary = [];
 let formHidden = true;
 
-function Book(title, author, pages, read) {
-    this.title = title;
-    this.author = author;
-    this.pages = pages;
-    this.read = read;
+class Book {
+    constructor(title, author, pages, read) {
+        this.title = title;
+        this.author = author;
+        this.pages = pages;
+        this.read = read;
+    }
 }
 
 function addBookToLibrary(title, author, pages, read) {
